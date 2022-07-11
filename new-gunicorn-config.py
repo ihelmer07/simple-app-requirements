3bd0ad7e332bff0127ee2347f25c97ddb3ffda49
Isaac Helmer <isaac.helmer@bsci.com>
2022-07-11 07:40:38 -0500

    back to gthread

--- new-gunicorn-config.py
+++ new-gunicorn-config.py
@@ -1,7 +1,8 @@
 worker_tmp_dir = "/dev/shm"
 bind = "0.0.0.0:8000"
-workers = 4
-worker_class = "uvicorn.workers.UvicornWorker"
+workers = 2
+threads = 4
+worker_class = "gthread"
 loglevel = "info"
 accesslog = "-"
 errorlog = "-"
